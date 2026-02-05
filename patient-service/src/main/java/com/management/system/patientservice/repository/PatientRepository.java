b6ccdba3a5794505b26a48214f9738623f9772fc
shubham thakur <shubhamthakur60278@gmail.com>
2026-02-06 00:07:40 +0530

    feat: add patient model and repository

---
+++ patient-service/src/main/java/com/management/system/patientservice/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package com.management.system.patientservice.repository;
+
+import com.management.system.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+}
