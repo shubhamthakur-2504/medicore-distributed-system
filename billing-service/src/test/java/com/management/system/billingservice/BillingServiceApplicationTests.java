7da6935448b57df4efe2bda15f5d33e16c4cff4d
shubham thakur <shubhamthakur60278@gmail.com>
2026-02-10 01:27:00 +0530

    setup: add billing-service setup

---
+++ billing-service/src/test/java/com/management/system/billingservice/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.system.billingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
